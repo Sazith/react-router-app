c9b49aef1f5330a3df4b28b69ccc0497ab9a4c0f
Sazith <sazith.simecsoftdev@gmail.com>
2025-04-29 12:12:06 +0600

    layout added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Home from './components/Home/Home'
 
 function App() {
 
   return (
     <>
    <Header/>
+   <Home/>
    <Footer/>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,34 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
+import Layout from './Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children:[
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <About/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter>
       <App />
-    </BrowserRouter>
+    </BrowserRouter> */}
+    <RouterProvider router={router}/>
   </StrictMode>,
 );
